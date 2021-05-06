4826316d819f84f44b62dc223a2a27a6595f3a64
mpchow <mattchow918@gmail.com>
2021-05-06 02:59:51 -0600

    Animate archived projects

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -9,7 +9,7 @@ class Home extends React.Component {
             <h1>Hi, I'm <br/> <span className="name">Matthew Chow</span> 👋🏻</h1>
             <h4>Studying Computer Engineering at <a href="https://www.ubc.ca" className="UBC" target="_blank" rel="noopener noreferrer">UBC</a></h4>
             <h4>Software Engineer Intern at <a href="https://league.com/" className="League" target="_blank" rel="noopener noreferrer">League</a></h4>
-            <h4>Former intern at <a href="https://www.sap.com/canada/" className="SAP" target="_blank" rel="noopener noreferrer">SAP</a>, <a href="http://perklabs.io" className="Perk" target="_blank" rel="noopener noreferrer">Perk Labs</a></h4>
+            <h4>Previously at <a href="https://www.sap.com/canada/" className="SAP" target="_blank" rel="noopener noreferrer">SAP</a>, <a href="http://perklabs.io" className="Perk" target="_blank" rel="noopener noreferrer">Perk Labs</a></h4>
             <h4>Focused on Backend Development and exploring Machine Learning</h4>
          </div>
       );
