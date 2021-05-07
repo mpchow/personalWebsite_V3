66c28d6ba0d57660a1dbaebc06643328ede68702
mpchow <mattchow918@gmail.com>
2021-05-06 18:27:07 -0600

    Initial transition

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import ScrollAnimation from 'react-animate-on-scroll';
 import './home.css';
 
-
+let delay = 500
+let interval = 150
+let speed = 0.5
 class Home extends React.Component {
    render() {
       return (
          <div className="home">
-            <h1>Hi, I'm <br/> <span className="name">Matthew Chow</span> 👋🏻</h1>
-            <h4>Studying Computer Engineering at <a href="https://www.ubc.ca" className="UBC" target="_blank" rel="noopener noreferrer">UBC</a></h4>
-            <h4>Software Engineer Intern at <a href="https://league.com/" className="League" target="_blank" rel="noopener noreferrer">League</a></h4>
-            <h4>Previously at <a href="https://www.sap.com/canada/" className="SAP" target="_blank" rel="noopener noreferrer">SAP</a>, <a href="http://perklabs.io" className="Perk" target="_blank" rel="noopener noreferrer">Perk Labs</a></h4>
-            <h4>Focused on Backend Development and exploring Machine Learning</h4>
+            <ScrollAnimation animateIn="fadeIn" animateOnce="True" duration="1.5">
+               <h1>Hi, I'm <br/> <span className="name">Matthew Chow</span> 👋🏻</h1>
+            </ScrollAnimation>
+
+            <ScrollAnimation animateIn="fadeInUp" animateOnce="True" delay={0 * interval + delay} duration={speed}>
+               <h4>Studying Computer Engineering at <a href="https://www.ubc.ca" className="UBC" target="_blank" rel="noopener noreferrer">UBC</a></h4> 
+            </ScrollAnimation>
+
+            <ScrollAnimation animateIn="fadeInUp" animateOnce="True" delay={1 * interval + delay} duration={speed}>
+               <h4>Software Engineer Intern at <a href="https://league.com/" className="League" target="_blank" rel="noopener noreferrer">League</a></h4>
+
+            </ScrollAnimation>
+
+            <ScrollAnimation animateIn="fadeInUp" animateOnce="True" delay={2 * interval + delay} duration={speed}>
+               <h4>Previously at <a href="https://www.sap.com/canada/" className="SAP" target="_blank" rel="noopener noreferrer">SAP</a>, <a href="http://perklabs.io" className="Perk" target="_blank" rel="noopener noreferrer">Perk Labs</a></h4>   
+            </ScrollAnimation>
+
+            <ScrollAnimation animateIn="fadeInUp" animateOnce="True" delay={3 * interval + delay} duration={speed}>
+               <h4>Focused on Backend Development and exploring Machine Learning</h4>
+            </ScrollAnimation>
+
          </div>
       );
    }
--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import { Link } from 'react-scroll';
+import ScrollAnimation from 'react-animate-on-scroll';
 import './sidebar.css';
 
+let interval = 150
+let delay = 1200
+let speed = 0.5
 
 class Sidebar extends React.Component {
    render() {
       return (
          <div className="sidebar">
-            <Link activeClass="active" to="About" spy={true} smooth={true} duration={1200}><h5 className="navigation">About</h5></Link>
-            <Link activeClass="active" to="Projects" spy={true} smooth={true} duration={1200}><h5 className="navigation">Projects</h5></Link>
-            <Link activeClass="active" to="Footer" spy={true} smooth={true} duration={1200}><h5 className="navigation">Contact</h5></Link>
-            <a href="/resume" target="_blank" rel="noopener noreferrer"><h5 className="navigation">Resume</h5></a>
+            <ScrollAnimation animateIn="fadeInRight" animateOnce="True" delay={0 * interval + delay} duration={speed}>
+               <Link activeClass="active" to="About" spy={true} smooth={true} duration={1200}>
+                  <h5 className="navigation">About</h5>
+               </Link>
+            </ScrollAnimation>
+
+            <ScrollAnimation animateIn="fadeInRight" delay={1 * interval + delay} animateOnce="True" duration={speed}>
+               <Link activeClass="active" to="Projects" spy={true} smooth={true} duration={1200}><h5 className="navigation">Projects</h5></Link>
+            </ScrollAnimation>
+
+            <ScrollAnimation animateIn="fadeInRight" delay={2 * interval + delay} animateOnce="True" duration={speed}> 
+               <Link activeClass="active" to="Footer" spy={true} smooth={true} duration={1200}><h5 className="navigation">Contact</h5></Link>
+            </ScrollAnimation>
+
+            <ScrollAnimation animateIn="fadeInRight" delay={3 * interval + delay} animateOnce="True" duration={speed}>
+               <a href="/resume" target="_blank" rel="noopener noreferrer"><h5 className="navigation">Resume</h5></a>
+            </ScrollAnimation>
          </div>
       );
    }
