46af94e1d5b89fe09175b29c8d30eab5ee3adb83
mpchow <mattchow918@gmail.com>
2021-08-23 20:21:14 -0600

    Remove logging

--- custom.webpack.config.js
+++ custom.webpack.config.js
@@ -11,15 +11,7 @@ new Promise((resolve) => {
       resolve(data)
    })
 }).then((file) => {
-    
-   let CodeAsString = ""
-   let regexCode = /\.\[hash\:8\]'/g
-   console.log(file)
-   // let regexCode = new RegExp('\.\[hash\:8\]');
-
-   //  let result = file.replace(regexCode, CodeAsString)
    let result = file.replace(`name: 'static/media/[name].[hash:8].[ext]'`, `name: 'static/media/[name].[ext]'`)
-   console.log(file)
 
     fs.writeFile(fileConfig, result, function (err) {
         if (err) return console.log(err)
